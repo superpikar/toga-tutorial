fcc36946ee33aeda8782a9e0154a51e690ad3a73
Dzulfikar Adi Putra <dzulfikar.adiputra@gmail.com>
2024-07-25 10:05:00 +0700

    feat: calculate function

--- calculate.py
+++ calculate.py
@@ -1,9 +1,16 @@
 import toga
-from toga.style.pack import COLUMN, LEFT, RIGHT, ROW, PACK
+from toga.style.pack import COLUMN, LEFT, RIGHT, ROW, Pack
 
 def build(app):
+    def calculate(widget):
+        try:
+            celcius_input.value = (float(fahrenheit_input.value) - 32) * 5 / 9
+        except ValueError:
+            celcius_input.value = "???"
+
     celcius_box = toga.Box()
     fahrenheit_box = toga.Box()
+    text_box = toga.Box()
     box = toga.Box()
 
     celcius_input = toga.TextInput(readonly=True)
@@ -11,6 +18,9 @@ def build(app):
 
     celcius_label = toga.Label("Celcius")
     fahrenheit_label = toga.Label("Fahrenheit")
+    text_label = toga.Label("is equivalent to", style=Pack(text_align=RIGHT))
+
+    button = toga.Button("Calculate", on_press=calculate)
 
     celcius_box.add(celcius_label)
     celcius_box.add(celcius_input)
@@ -18,12 +28,18 @@ def build(app):
     fahrenheit_box.add(fahrenheit_label)
     fahrenheit_box.add(fahrenheit_input)
 
-    box.add(celcius_box)
+    text_box.add(text_label)
+
     box.add(fahrenheit_box)
+    box.add(text_box)
+    box.add(celcius_box)
+    box.add(button)
 
     box.style.update(direction=COLUMN, padding=10)
+    button.style.update(padding=15)
 
     return box
+
 
 def main():
     return toga.App("Temperature converter", "org.beeware.toga.tutorial", startup=build)
